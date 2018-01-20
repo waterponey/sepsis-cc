c4191301da3fff8d14d7cb5429229615255088c9
Dekken <philip.deegan@gmail.com>
2018-01-20 16:50:04 +0100

    Create septic_shock.py

---
+++ sql/omop/cohort/septic_shock.py
@@ -0,0 +1,10 @@
+--2586 occurrences
+SELECT c.concept_name, COUNT(*)
+FROM condition_occurrence co, visit_occurrence vo, concept c 
+where 
+co.condition_source_concept_id = c.concept_id
+and co.visit_occurrence_id = vo.visit_occurrence_id
+and lower(c.concept_name) like lower('%septic%')
+group by c.concept_name  ORDER BY count(1) desc 
+limit 1000
+
